2614f149e7caad19cc363428a107a1f46141f347
amitaucs <the.amisoft@gmail.com>
2020-05-10 18:51:42 +0200

    stub corrected

--- src/main/java/com/digi/trade/TradeSettlementController.java
+++ src/main/java/com/digi/trade/TradeSettlementController.java
@@ -1,10 +1,12 @@
 package com.digi.trade;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,10 +14,18 @@ public class TradeSettlementController {
 
     private final RestTemplate restTemplate;
 
+    @Value("${tradeverifier.baseurl}")
+    private  String tradeVerifierBaseUrl;
+
     @PostMapping("/trade-settlement")
     public ForTradeSettlementResponse applyForTradeSettlement(@RequestBody  final ForTradeSettlementRequest forTradeSettlementRequest){
+
+        final String uri = UriComponentsBuilder.fromHttpUrl(tradeVerifierBaseUrl)
+                .path("trade-verify")
+                .toUriString();
+
          final long tradeRefNumber = forTradeSettlementRequest.getTradeRefNumber();
-         final TradeVerifyResponse tradeVerifyResponse = restTemplate.postForObject("http://localhost:9001/trade-verify",
+         final TradeVerifyResponse tradeVerifyResponse = restTemplate.postForObject(uri,
                  new TradeVerifyRequest(tradeRefNumber),TradeVerifyResponse.class);
          if(tradeVerifyResponse.getTradeVolumeType()== TradeVerifyResponse.TradeVolumeType.HIGH
          && forTradeSettlementRequest.getBrokerageType() == ForTradeSettlementRequest.BrokerageType.PLATINUM){
